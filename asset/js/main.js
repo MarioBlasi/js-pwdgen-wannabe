3ba78706d18147f478b89aaee00399e9b75677b1
MarioBlasi <mblasi509@gmail.com>
2023-03-07 12:54:28 +0000

    prompt ask user data

--- asset/js/main.js
+++ asset/js/main.js
@@ -1,1 +1,20 @@
 alert("dovrebbefunzionare");
+//Chiediamo all'utente il suo nome
+//Chiediamo all'utente il suo Cognome
+//chiediamo all'utente il suo colore preferito
+//Stampo a schermo il risultato usando: nomecognomecolorepreferito23
+/*-------------------------*/
+
+//Uso il prompt per chiedere il Nome all'utente salvandola in una variabile
+//console.log il risultato del prompt
+// Inserisco un h1 nell'html
+
+//Uso il prompt per chiedere il Cognome all'utente salvandola in una variabile
+//console.log il risultato del prompt
+// Inserisco un h1 nell'html
+
+//Uso il prompt per chiedere il Colore preferito all'utente salvandola in una variabile
+//console.log il risultato del prompt
+// Inserisco un h1 nell'html
+
+//Seleziono e salvo in una variabiletutti e tre gli elementi della dom con getElementById
