6b5cfda1a4332d1294c95051ba7d82715619a61c
MarioBlasi <mblasi509@gmail.com>
2023-03-07 17:12:48 +0000

    const  //let

--- asset/js/main.js
+++ asset/js/main.js
@@ -1,4 +1,4 @@
-alert("dovrebbefunzionare");
+alert("welcome");
 //Chiediamo all'utente il suo nome
 //Chiediamo all'utente il suo Cognome
 //chiediamo all'utente il suo colore preferito
@@ -7,33 +7,32 @@ alert("dovrebbefunzionare");
 /*-------------------------*/
 
 //Uso il prompt per chiedere il Nome all'utente salvandola in una variabile
-let username = prompt("inserisci il tuo nome");
+const username = prompt("inserisci il tuo nome");
 //console.log il risultato del prompt
 console.log(username);
 // Inserisco un h1 nell'html
 /*---------NAME-----*/
 
 //Uso il prompt per chiedere il Cognome all'utente salvandola in una variabile
-let surname = prompt("inserisci il tuo cognome");
+const surname = prompt("inserisci il tuo cognome");
 //console.log il risultato del prompt
 console.log(surname);
 // Inserisco un h1 nell'html
 /*---------SURNAME-----*/
 
 //Uso il prompt per chiedere il Colore preferito all'utente salvandola in una variabile
-let favoritecolor = prompt("inserisci il tuo colore prefetito");
+const favoritecolor = prompt("inserisci il tuo colore prefetito");
 //console.log il risultato del prompt
 console.log(favoritecolor);
 // Inserisco un h1 nell'html
 /*---------FAVORITE_COLOR----*/
 
-/*//addizioniamo usando il segno +
-let somma = "name" + "surname" + "fcolor" + 23;
-let result = somma;*/
-
-//seleziono e salvo in una variabile un elemento della doma con getElementById
-let nameElement = document.getElementById("name");
+//addizioniamo usando il segno +
+const somma = username + surname + favoritecolor + 23;
+//seleziono e salvo in una variabile un elemento della dom con getElementById
+const nameElement = document.getElementById("name");
 console.log(nameElement);
 
 //assegno all'innerHTML un nuovo valore
-nameElement.innerHTML = ` ${username + surname + favoritecolor + 23}`;
+//Stampo a schermo il risultato usando: nomecognomecolorepreferito23
+nameElement.innerHTML = somma;
