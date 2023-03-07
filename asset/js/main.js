5f4e7412ccbf4fecd20ef60c97e0a47d848ffd97
MarioBlasi <mblasi509@gmail.com>
2023-03-07 15:11:29 +0000

    nomecognomecolorepreferito23

--- asset/js/main.js
+++ asset/js/main.js
@@ -2,35 +2,49 @@ alert("dovrebbefunzionare");
 //Chiediamo all'utente il suo nome
 //Chiediamo all'utente il suo Cognome
 //chiediamo all'utente il suo colore preferito
+//chiediamo all'utente la sua etá
 //Stampo a schermo il risultato usando: nomecognomecolorepreferito23
 /*-------------------------*/
 
 //Uso il prompt per chiedere il Nome all'utente salvandola in una variabile
-let username = prompt("inserisci il tuo nome");
+let userName = prompt("inserisci il tuo nome");
 //console.log il risultato del prompt
-console.log("username");
+console.log(userName);
 // Inserisco un h1 nell'html
 /*---------NAME-----*/
 
 //Uso il prompt per chiedere il Cognome all'utente salvandola in una variabile
-let surname = prompt("inserisci il tuo cognome");
+let surName = prompt("inserisci il tuo cognome");
 //console.log il risultato del prompt
-console.log("surname");
+console.log(surName);
 // Inserisco un h1 nell'html
 /*---------SURNAME-----*/
 
 //Uso il prompt per chiedere il Colore preferito all'utente salvandola in una variabile
 let favoritecolor = prompt("inserisci il tuo colore prefetito");
 //console.log il risultato del prompt
-console.log("favoritecolor");
+console.log(favoritecolor);
 // Inserisco un h1 nell'html
 /*---------FAVORITE_COLOR----*/
 
-//creo variabile con addizzione
+//Uso il prompt per chiedere il Nome all'utente salvandola in una variabile
+let userAge = prompt("inserisci la tua etá");
+//console.log il risultato del prompt
+console.log(userAge);
+// Inserisco un h1 nell'html
+/*---------AGE-----*/
 
-//Seleziono e salvo in una variabile l'elemento "name" della dom con getElementById
-let nameElement = document.getElementById("name");
-//Assegno all'innerHTML un nuovo valore
+//addizioniamo usando il segno +
+let somma = "userName" + "surName" + "favoritecolor" + "userAge";
+console.log(somma);
 
-//Addiziono tutti e tre gli elementi
+/* Template literal 
+const userName = "Mario";
+const surName = "Blasi";
+const favoritecolor = "red";
+const userAge = 23;
+
+const sentence = `mi chiamo ${username}, ${surname} il mio colore preferito é il ${favoritecolor}${userage}`;
+console.log(sentence);*/
+
 //Stampo a schermo il risultato usando: nomecognomecolorepreferito23
